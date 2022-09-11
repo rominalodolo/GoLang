83de0526426999f380b166a1c5e94b8ae961bdec
Romina <lodoloromina@gmail.com>
2022-09-11 19:48:51 +0200

    update

--- goquizapp/main.go
+++ goquizapp/main.go
@@ -6,9 +6,23 @@ package main
 
 // import "github.com/golang/goquizapp/cmd" 
 import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl *template.Template
 
 func main() {
 	// cmd.Execute()
 
-	fmt.Println("Hello, World!")
+	
+	mux := http.NewServeMux()
+	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+
+	fmt.Println("You scored higher than 60% of all quizzers")
+
+	log.Fatal(http.ListenAndServe(":9091", mux))
+
 }
