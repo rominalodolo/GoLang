159b42aa3a6e7274d1996ca80aa8f49c25bf431d
Romina <lodoloromina@gmail.com>
2022-09-11 21:30:55 +0200

    update

--- goquizapp/main.go
+++ goquizapp/main.go
@@ -5,24 +5,44 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package main
 
 // import "github.com/golang/goquizapp/cmd" 
-import "fmt"
 import (
+	"fmt"
 	"html/template"
-	"log"
+	// "log"
 	"net/http"
+	"errors"
+	"io"
+	"os"
 )
 
 var tmpl *template.Template
 
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Welcome to the South African Whales and Dolphin Quiz!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
 func main() {
 	// cmd.Execute()
 
-	
-	mux := http.NewServeMux()
 	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 
 	fmt.Println("You scored higher than 60% of all quizzers")
 
-	log.Fatal(http.ListenAndServe(":9091", mux))
+
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 
 }
--- goquizapp/static/js/script.js
+++ goquizapp/static/js/script.js
@@ -1,8 +1,8 @@
 
-// let items = document.getElementsByTagName("li")
+let items = document.getElementsByTagName("li")
 
-// for (let i=0; i<items.length; i++) {
-//     items[i].addEventListener("click", () => {
-//         items[i].classList.toggle("done")
-//     })
-// }
+for (let i=0; i<items.length; i++) {
+    items[i].addEventListener("click", () => {
+        items[i].classList.toggle("done")
+    })
+}
